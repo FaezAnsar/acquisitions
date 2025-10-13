dbb25b92cff2019d5d7db75a49f34ca32f868208
FaezAnsar <FAEZANSARAHMED2004@GMAIL.COM>
2025-10-13 15:31:41 +0500

    securing with arcjet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRoutes from '#routes/auth.routes';
+import  securityMiddleware  from '#middlewares/security.middleware';
 const app = express();
+
 
 app.use(helmet());
 app.use(cors());
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()  );
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+app.use(securityMiddleware);
 app.get('/', (req, res) => {
   logger.info('Hello from acquisitions api ');
   res.status(200).send('Hello from acquisitions api!');
---
+++ src/middlewares/security.middleware.js
@@ -0,0 +1,52 @@
+import aj from '#config/arcjet' 
+import logger from '#config/logger';
+import { slidingWindow } from '@arcjet/node';
+
+const securityMiddleware = async(req, res, next) => {
+    try {
+        const role= req.user?.role || 'guest';
+        let limit;
+        let message;
+
+        switch (role) {
+            case 'admin':
+                limit = 20
+                message= 'Admin req limit exceeded (20 requests per minute)';
+                break;
+            case 'user':
+                limit = 10;
+                message= 'User req limit exceeded (10 requests per minute)';
+                break;
+            case 'guest'            :
+                limit = 5;
+                message= 'Guest req limit exceeded (5 requests per minute)';
+                break;
+        }
+        const client = aj.withRule(slidingWindow({mode:'LIVE',interval:1,max:limit, name:`${role}-limit`}));
+        const decision = await client.protect(req);
+        if (decision.isDenied()&& decision.reason.isBot()) {
+            logger.warn(`Bot req blocked`, {ip:req.ip, userAgent:req.get('User-Agent'),path:req.path});
+            return res.status(403).json({ error: 'Forbidden',message:'Bot requests are not allowed' });
+        
+        }
+        if (decision.isDenied()&& decision.reason.isShield()) {
+            logger.warn(`Shield req blocked`, {ip:req.ip, userAgent:req.get('User-Agent'),path:req.path,method:req.method});
+            return res.status(403).json({ error: 'Forbidden',message:'Shield requests are not allowed' });
+
+        }
+        if (decision.isDenied()&& decision.reason.isRateLimit()) {
+            logger.warn(`Rate limit exceeded`, {ip:req.ip, userAgent:req.get('User-Agent'),path:req.path});
+            return res.status(403).json({ error: 'Forbidden',message:'Rate limit exceeded' });
+
+        }
+        next();
+    
+    }catch(e){
+        console.error("Arcjet middleware error:", e);
+        res.status(500).json({ error: 'Internal server error' });
+
+    }
+
+}
+export default securityMiddleware ;
+            
